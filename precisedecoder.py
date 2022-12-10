6ded32567470c64d095acace2868c593ac3b24d7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 18:17:17 +0000

    Adding tarfile member sanitization to extractall()

--- precisedecoder.py
+++ precisedecoder.py
@@ -128,7 +128,26 @@ class HotwordDetector(Thread):
                         f.write(file.content)
 
                     with tarfile.open(filepath) as tar:
-                        tar.extractall(path=TOP_DIR)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar, path=TOP_DIR)
                     os.remove(filepath)
                     return True
         return False
